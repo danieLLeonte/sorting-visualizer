2b2290f5438fbe3fb3f1a103581b009beaba185b
Daniel Leonte <leontedaniel37@gmail.com>
2023-09-28 13:00:49 +0300

    handle if sorting or sorted array

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,23 @@ const ANIMATION_SPEED_MS = 150;
 const PRIMARY_COLOR = "turquoise";
 const SECONDARY_COLOR = "red";
 
+let isSorting = false;
+let isSorted = false;
+
 const App = () => {
-  const [array, setArray] = useState(resetArray());
+  const [array, setArray] = useState(resetArray);
 
   const handleResetArray = () => {
-    setArray(resetArray());
+    if (isSorting) return;
+    setArray(resetArray);
+    isSorted = false;
   };
 
   const mergeSort = () => {
+    // if already sorting or already sorted, return
+    if (isSorting || isSorted) return;
+    isSorting = true;
+
     const animations = getMergeSortAnimations(array);
     for (let i = 0; i < animations.length; i++) {
       const arrayBars = document.getElementsByClassName("array-bar");
@@ -37,6 +46,11 @@ const App = () => {
         }, (i * ANIMATION_SPEED_MS) / (array.length / 2));
       }
     }
+    // set isSorting to false after the animations are done
+    setTimeout(() => {
+      isSorting = false;
+      isSorted = true;
+    }, (animations.length * ANIMATION_SPEED_MS) / (array.length / 2));
   };
 
   useEffect(() => {
@@ -48,7 +62,7 @@ const App = () => {
   return (
     <div className="flex flex-col gap-6">
       <Navbar handleResetArray={handleResetArray} mergeSort={mergeSort} />
-      <main className="mt-2 flex justify-center">
+      <main className="mt-2 flex justify-center overflow-x-scroll mx-auto">
         {array.map((value, idx) => (
           <div
             className="w-[2px] mx-[2px] inline-block array-bar"
