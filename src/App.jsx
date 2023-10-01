540a233bf5f5327b311da0c6ef6c639f52ba4e57
Daniel Leonte <leontedaniel37@gmail.com>
2023-10-01 14:24:47 +0300

    refactor mergeSort

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Navbar from "./components/Navbar";
 import { resetArray } from "./utils";
 import { getMergeSortAnimations } from "./algorithms/mergeSort";
 
-const ANIMATION_SPEED_MS = 0.5;
+const ANIMATION_SPEED_MS = 0.1;
 const PRIMARY_COLOR = "turquoise";
 const SECONDARY_COLOR = "red";
 
 let isSorting = false;
 let isSorted = false;
+let animationIndex = 0;
+let lastTimestamp = 0;
 
 const App = () => {
   const [array, setArray] = useState(resetArray);
@@ -22,46 +24,56 @@ const App = () => {
   };
 
   const mergeSort = () => {
-    // Check if it's already sorting or sorted
     if (isSorting || isSorted) return;
-  
+
     isSorting = true;
     const animations = getMergeSortAnimations(array);
+    const interval = ANIMATION_SPEED_MS / (array.length * 2);
     const arrayBars = containerRef.current.children;
-  
-    const executeAnimation = (index) => {
-      if (index >= animations.length) {
-        isSorting = false;
-        isSorted = true;
-        return;
+
+    const animate = (timestamp) => {
+      // Check if enough time has passed since the last animation step
+      if (timestamp - lastTimestamp > interval) {
+        lastTimestamp = timestamp;
+
+        const isColorChange = animationIndex % 3 !== 2;
+        if (isColorChange) {
+          const [barOneIdx, barTwoIdx] = animations[animationIndex];
+          const barOneStyle = arrayBars[barOneIdx].style;
+          const barTwoStyle = arrayBars[barTwoIdx].style;
+          const color =
+            animationIndex % 3 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
+
+          barOneStyle.backgroundColor = color;
+          barTwoStyle.backgroundColor = color;
+        } else {
+          const [barOneIdx, newHeight] = animations[animationIndex];
+          const barOneStyle = arrayBars[barOneIdx].style;
+
+          barOneStyle.height = `${newHeight}px`;
+          setArray((prevArray) => {
+            const newArray = [...prevArray];
+            newArray[barOneIdx] = newHeight;
+            return newArray;
+          });
+        }
+
+        animationIndex++;
+
+        // Check if we've reached the end of our animations
+        if (animationIndex >= animations.length) {
+          isSorting = false;
+          isSorted = true;
+          return; // Stop the animation
+        }
       }
-  
-      const isColorChange = index % 3 !== 2;
-      if (isColorChange) {
-        const [barOneIdx, barTwoIdx] = animations[index];
-        const barOneStyle = arrayBars[barOneIdx].style;
-        const barTwoStyle = arrayBars[barTwoIdx].style;
-        const color = index % 3 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
-  
-        barOneStyle.backgroundColor = color;
-        barTwoStyle.backgroundColor = color;
-      } else {
-        const [barOneIdx, newHeight] = animations[index];
-        const barOneStyle = arrayBars[barOneIdx].style;
-  
-        barOneStyle.height = `${newHeight}px`;
-        setArray((prevArray) => {
-          const newArray = [...prevArray];
-          newArray[barOneIdx] = newHeight;
-          return newArray;
-        });
-      }
-  
-      setTimeout(() => executeAnimation(index + 1), ANIMATION_SPEED_MS / array.length);
+
+      // Schedule the next frame
+      requestAnimationFrame(animate);
     };
-  
-    // Start the animation sequence
-    executeAnimation(0);
+
+    // Start the animation
+    requestAnimationFrame(animate);
   };
 
   useEffect(() => {
@@ -79,7 +91,7 @@ const App = () => {
       >
         {array.map((value, idx) => (
           <div
-            className="w-[2px] mx-[2px] inline-block array-bar"
+            className="w-2 mx-0.5 inline-block array-bar"
             key={idx}
             style={{
               backgroundColor: PRIMARY_COLOR,
--- src/utils/index.js
+++ src/utils/index.js
@@ -8,7 +8,7 @@ export function resetArray() {
   const windowWidth = window.innerWidth;
   const windowHeight = window.innerHeight;
   const MAX_HEIGHT_BAR = windowHeight - 160;
-  const NUMBER_OF_ARRAY_BARS = Math.floor(windowWidth / 6.1);
+  const NUMBER_OF_ARRAY_BARS = Math.floor(windowWidth / 12.2);
   const array = [];
   for (let i = 0; i < NUMBER_OF_ARRAY_BARS; i++) {
     array.push(randomIntFromInterval(5, MAX_HEIGHT_BAR));
